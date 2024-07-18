f29ee4685397871c9b0f43c7a8cce9324d84dfb7
K-Shravya-Shetty <21j14.shravya@sjec.ac.in>
2024-07-18 21:58:38 +0530

    readme file modified

---
+++ patterns.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern1(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void pattern2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void pattern3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    //pattern1(3);
+    //pattern2(3);
+    //pattern3(5);
+}
