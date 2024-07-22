a81ea6d10317af6af03009d91f11786c22c61185
K-Shravya-Shetty <21j14.shravya@sjec.ac.in>
2024-07-22 21:03:32 +0530

    some more problems added

--- basicprobs.cpp
+++ basicprobs.cpp
@@ -51,10 +51,42 @@ void palindrome(int num){
         cout<<"the number is not a palindrome ";
     }
 }
+//swapping two numbers
+void swapnum(int a ,int b){
+    int temp;
+    temp =b;
+    b=a;
+    a=temp;
+    cout<<a<<endl;
+    cout<<b;
+}
+//checking the number is prime or not
+void check_prime(int num){
+    int count=0;
+    for(int i=1;i<=num;i++){
+        if(num%i==0){
+            count=count+1;
+        }
+    }
+    if(count==2){
+        cout<<"the number is prime";
+    }
+    else{
+        cout<<"the number is not prime";
+    }
+}
+//check the number is a armstrong number
+//armstrong number is a number that is equal to the sum of its digits raised to the power of the number of digits
+void armstrong_num(int num){
+
+}
 int main(){
     //extract(1267);
     //count_dig(98873);
     //rev_num(7654);
     //palindrome(121);
     //palindrome(123);
+    //swapnum(4,5);
+    //check_prime(4);
+    //check_prime(7);
 }
