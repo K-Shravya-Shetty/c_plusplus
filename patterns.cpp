07571c104d5de4938090c3e8a790244d3a1a7bac
K-Shravya-Shetty <21j14.shravya@sjec.ac.in>
2024-07-22 21:04:40 +0530

    patterns added

--- patterns.cpp
+++ patterns.cpp
@@ -66,6 +66,70 @@ void pattern7(int n){
         cout<<endl;
     }
 }
+void pattern8(int n){
+    for(int i=0;i<n;i++){
+        //for spaces on to the left
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        //for spaces on to the right
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern9(int n){
+    for(int i=0;i<n;i++){
+        //for space on to the left side
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //for the stars ot the pattern
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        //for space on to the right side
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++){
+        //for spaces on to the left
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        //for spaces on to the right
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern10(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void pattern11(int n){
+    
+}
 int main(){
     //pattern1(3);
     //pattern2(3);
@@ -74,4 +138,7 @@ int main(){
     //pattern5(4);
     //pattern6(4);
     //pattern7(3);
+    //pattern8(3);
+    //pattern9(3);
+    //pattern10(3);
 }
