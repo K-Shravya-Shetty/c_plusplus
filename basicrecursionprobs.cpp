5441babe9070cc0f4d5f747fc35d40fe27154fe2
K-Shravya-Shetty <21j14.shravya@sjec.ac.in>
2024-08-30 12:03:55 +0530

    printing n numbers linearly in reverse order using recursion

--- basicrecursionprobs.cpp
+++ basicrecursionprobs.cpp
@@ -2,29 +2,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 // print the name n times using recursion
-void  printName(int i, int n)
+void printName(int i, int n)
 {
-    if ( i > n)
+    if (i > n)
     {
         return;
     }
-    cout << "hello world"<<endl;
+    cout << "hello world" << endl;
     printName(i + 1, n);
 }
-//print the numbers linearly from 1 to n using recursion
-void printlinerly(int i,int n){
-    if (i>n){
+// print the numbers linearly from 1 to n using recursion
+void printlinerly(int i, int n)
+{
+    if (i > n)
+    {
         return;
     }
-        cout<<i<<endl;
-        printlinerly(i+1,n);
+    cout << i << endl;
+    printlinerly(i + 1, n);
+}
+void print(int i, int n)
+{
+    if (i < 1)
+    {
+        return;
+    }
+    cout << i<<endl;
+    print(i - 1, n);
 }
 int main()
 {
     int n;
-    cout<<"enter the value of n ";
+    cout << "enter the value of n ";
     cin >> n;
-    //printName(1, n);
-    printlinerly(1,n);
+    // printName(1, n);
+    // printlinerly(1,n);
+    print(n,n);
     return 0;
 }
