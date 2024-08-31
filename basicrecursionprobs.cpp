260f2d02fc41b9961303c97214c01b50f694632f
K-Shravya-Shetty <21j14.shravya@sjec.ac.in>
2024-08-31 20:25:51 +0530

    all changes

--- basicrecursionprobs.cpp
+++ basicrecursionprobs.cpp
@@ -21,62 +21,73 @@ void printinc(int i, int n)
     cout << i << endl;
     printinc(i + 1, n);
 }
-//print n numbers in the decreasing order sing recursion
+// print n numbers in the decreasing order sing recursion
 void printdec(int i, int n)
 {
     if (i < 1)
     {
         return;
     }
-    cout << i<<endl;
+    cout << i << endl;
     printdec(i - 1, n);
 }
-//print n numbers in increasing order using backtracking method
-void backprintinc(int i,int n){
-    if(i<1){
-        return;
-    }
-    backprintinc(i-1,n);
-    cout<<i<<endl;
-}
-//print n numbers in decreasing order using backtracking method
-void backprintdec(int i,int n){
-    if(i>n){
-        return;
-    }
-    backprintdec(i+1,n);
-    cout<<i<<endl;
-}
-//print the sum of first n numbers using the parameter method
-void printsum(int i,int sum){
-    if(i<1){
-        cout<<sum;
-        return ;
-    }
-    printsum(i-1,sum+i);
-}
-//print the sum of first n numbers using the function method
-int fun(int i){
-    if(i==0) return 0;
-return i+fun(i-1) ;
-}
-int fact(int i){
-    if(i==0) return 0;
-    if(i==1) return 1;
-return i*fact(i-1);
-}
-int main()
+// print n numbers in increasing order using backtracking method
+void backprintinc(int i, int n)
 {
-    int n;
-    cout << "enter the value of n ";
-    cin >> n;
+    if (i < 1)
+    {
+        return;
+    }
+    backprintinc(i - 1, n);
+    cout << i << endl;
+}
+// print n numbers in decreasing order using backtracking method
+void backprintdec(int i, int n)
+{
+    if (i > n)
+    {
+        return;
+    }
+    backprintdec(i + 1, n);
+    cout << i << endl;
+}
+// print the sum of first n numbers using the parameter method
+void printsum(int i, int sum)
+{
+    if (i < 1)
+    {
+        cout << sum;
+        return;
+    }
+    printsum(i - 1, sum + i);
+}
+// print the sum of first n numbers using the function method
+int fun(int i)
+{
+    if (i == 0)
+        return 0;
+    return i + fun(i - 1);
+}
+// print the factorial of the number using the function method
+int fact(int i)
+
+{
+    if (i == 0)
+        return 0;
+    if (i == 1)
+        return 1;
+    return i * fact(i - 1);
+}
+
+int main()
+{   
     // printName(1, n);
     // printinc(1,n);
-    //printdec(n,n);
-    //backprintinc(n,n);
-    //backprintdec(1,n);
-    //printsum(n,0);
-    //cout<<fun(n);
-    cout<<fact(n);
+    // printdec(n,n);
+    // backprintinc(n,n);
+    // backprintdec(1,n);
+    // printsum(n,0);
+    // cout<<fun(n);
+    // cout << fact(n);
     return 0;
 }
