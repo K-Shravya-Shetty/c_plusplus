6c9e61eeab1ae04ba75b594ec3e43dc755d4d7f1
K-Shravya-Shetty <21j14.shravya@sjec.ac.in>
2024-09-02 09:12:28 +0530

    left rotate the array by d values

--- arrays.cpp
+++ arrays.cpp
@@ -1,3 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int arr[];
+void leftRotate(int arr[],int n,int d){
+    reverse(arr,arr+d);
+    reverse(arr+d,arr+n);
+    reverse(arr,arr+n);
+}
+int main(){
+    int n;
+    cout<<"enter the value of n : ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the array elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int d;
+    cout<<"enter the value of d: ";
+    cin>>d;
+    leftRotate(arr,n,d);
+    cout<<"array after the left shift ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
