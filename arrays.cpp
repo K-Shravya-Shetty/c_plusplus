8261ca8606e9c94751be53064e595f3582a935e9
K-Shravya-Shetty <21j14.shravya@sjec.ac.in>
2024-09-02 09:19:10 +0530

    linear search

--- arrays.cpp
+++ arrays.cpp
@@ -5,6 +5,13 @@ void leftRotate(int arr[],int n,int d){
     reverse(arr+d,arr+n);
     reverse(arr,arr+n);
 }
+void linearSearch(int arr[],int n,int d){
+    for(int i=0;i<n;i++){
+        if(arr[i]==d){
+            cout<<"the number is found at the index "<<i;
+        }
+    }
+}
 int main(){
     int n;
     cout<<"enter the value of n : ";
@@ -17,10 +24,11 @@ int main(){
     int d;
     cout<<"enter the value of d: ";
     cin>>d;
-    leftRotate(arr,n,d);
-    cout<<"array after the left shift ";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+    linearSearch(arr,n,d);
+    // leftRotate(arr,n,d);
+    // cout<<"array after the left shift ";
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
     return 0;
 }
